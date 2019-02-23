c591d9ca0a02602de388454fab0b987d92df2bc0
Artem <artem.fufaldin@gmail.com>
2019-02-24 01:41:11 +0500

    Change table to grid #21

--- application/views/ac/add_personal.php
+++ application/views/ac/add_personal.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
             <div id="main">
                 <div class="info-container">
-                    <div class="info-item info-item-photo">
+                    <div id="info-item-photo" class="info-item">
                         <div id="photo_bg" class="photo">
                               <div id="photo_del" data-title="<?php echo lang('delete')?>" hidden>
                                   <img src="/img/delete.png" />
@@ -13,38 +13,38 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         <?php echo lang('class');?><br />
                         <?php echo form_dropdown('class', $classes, '0', $class_attr);?>
                     </div>
-                    <div class="info-item info-item-f">
+                    <div id="info-item-f" class="info-item">
                         <?php echo lang('f');?><br />
                         <input maxlength="20" id="f" name="f" size="30" type="text" required />
                     </div>
-                    <div class="info-item info-item-i">
+                    <div id="info-item-i" class="info-item">
                         <?php echo lang('i');?><br />
                         <input maxlength="20" id="i" name="i" size="30" type="text" required />
                     </div>
-                    <div class="info-item info-item-o">
+                    <div id="info-item-o" class="info-item">
                         <?php echo lang('o');?><br />
                         <input maxlength="20" id="o" name="o" size="30" type="text" />
                     </div>
-                    <div class="info-item info-item-birthday">
+                    <div id="info-item-birthday" class="info-item">
                         <?php echo lang('birthday');?><br />
                         <input maxlength="20" id="birthday" name="birthday" size="15" type="date" required />
                     </div>
-                    <div class="info-item info-item-address">
+                    <div id="info-item-address" class="info-item">
                         <?php echo lang('address');?><br />
                         <input maxlength="50" id="address" name="address" size="60" type="text" />
                     </div>
-                    <div class="info-item info-item-phone">
+                    <div id="info-item-phone" class="info-item">
                         <?php echo lang('phone');?><br />
                         <input maxlength="10" id="phone" name="phone" size="15" type="text" />
                     </div>
-                    <div class="info-item info-item-card">
+                    <div id="info-item-card" class="info-item">
                         <?php echo lang('card');?><br />
                           <?php echo form_dropdown('card_menu', $cards, '0', $card_attr);?>
                     </div>
-                    <div class="info-item info-item-button1">
+                    <div id="info-item-button1" class="info-item">
                         <button type="button" onclick="savePersInfo(true);"><?php echo lang('save');?></button>
                     </div>
-                    <div class="info-item info-item-button2">
+                    <div id="info-item-button2" class="info-item">
                         <button type="button" onclick="savePersInfo(false);"><?php echo lang('clear');?></button>
                     </div>
                 </div>
--- application/views/ac/edit_personal.php
+++ application/views/ac/edit_personal.php
@@ -2,13 +2,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
             <div id="main">
-                <div class="main-container">
+                <div class="container">
                     <div id="menu" onclick="tree_toggle(arguments[0]);">
                         <?php echo $menu;?>
                     </div>
                     <div id="info">
-                        <div class="info-container">
-                            <div class="info-item info-item-photo">
+                        <div id="info-container">
+                            <div id="info-item-photo" class="info-item">
                                 <div id="photo_bg" class="photo">
                                     <div id="photo_del" data-title="<?php echo lang('delete')?>" hidden>
                                         <img src="/img/delete.png" />
@@ -18,41 +18,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <?php echo lang('class');?><br />
                                 <?php echo form_dropdown('class', $classes, '0', $class_attr);?>
                             </div>
-                            <div class="info-item info-item-f">
+                            <div id="info-item-f" class="info-item">
                                 <?php echo lang('f');?><br />
                                 <input maxlength="20" id="f" name="f" size="30" type="text" required readonly />
                             </div>
-                            <div class="info-item info-item-i">
+                            <div id="info-item-i" class="info-item">
                                 <?php echo lang('i');?><br />
                                 <input maxlength="20" id="i" name="i" size="30" type="text" required readonly />
                             </div>
-                            <div class="info-item info-item-o">
+                            <div id="info-item-o" class="info-item">
                                 <?php echo lang('o');?><br />
                                 <input maxlength="20" id="o" name="o" size="30" type="text" readonly />
                             </div>
-                            <div class="info-item info-item-birthday">
+                            <div id="info-item-birthday" class="info-item">
                                 <?php echo lang('birthday');?><br />
                                 <input maxlength="20" id="birthday" name="birthday" size="15" type="date" required readonly />
                             </div>
-                            <div class="info-item info-item-address">
+                            <div id="info-item-address" class="info-item">
                                 <?php echo lang('address');?><br />
                                 <input maxlength="50" id="address" name="address" size="60" type="text" readonly />
                             </div>
-                            <div class="info-item info-item-phone">
+                            <div id="info-item-phone" class="info-item">
                                 <?php echo lang('phone');?><br />
                                 <input maxlength="10" id="phone" name="phone" size="15" type="text" readonly />
                             </div>
-                            <div class="info-item info-item-card">
+                            <div id="info-item-card" class="info-item">
                                 <?php echo lang('card');?><br />
                                 <div id="cards"></div>
                                 <div id="card_selector">
                                     <?php echo form_dropdown('card_menu', $cards, '0', $card_attr);?>
                                 </div>
                             </div>
-                            <div class="info-item info-item-button1">
+                            <div id="info-item-button1" class="info-item">
                                 <button id="save" type="button"><?php echo lang('save');?></button>
                             </div>
-                            <div class="info-item info-item-button2">
+                            <div id="info-item-button2" class="info-item">
                                 <button id="delete" type="button"><?php echo lang('delete');?></button>
                             </div>
                         </div>
