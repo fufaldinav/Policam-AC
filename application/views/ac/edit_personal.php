6804d0a74857f986d4ea501981af55e99ea17870
Artem <artem.fufaldin@gmail.com>
2019-02-23 22:00:38 +0500

    Change table to grid #3

--- application/views/ac/edit_personal.php
+++ application/views/ac/edit_personal.php
@@ -7,45 +7,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           </div>
           <div id="main">
               <div class="grid-container">
-                  <form name="pers_info" target="hidden">
-                      <div class="grid-item">
-                          <div id="photo_bg" class="photo"><div id="photo_del" data-title="<?php echo lang('delete')?>" hidden><img src="/img/delete.png" /></div></div>
-                          <input id="photo" name="photo" type="file" hidden /><br />
-                          <?php echo lang('class');?><br />
-                          <?php echo form_dropdown('class', $classes, '0', $class_attr);?>
-                      </div>
-                      <div class="grid-item">
-                          <?php echo lang('f');?><br />
-                          <input maxlength="20" id="f" name="f" size="30" type="text" required readonly />
-                      </div>
-                      <div class="grid-item">
-                          <?php echo lang('i');?><br />
-                          <input maxlength="20" id="i" name="i" size="30" type="text" required readonly />
-                      </div>
-                      <div class="grid-item">
-                          <?php echo lang('o');?><br />
-                          <input maxlength="20" id="o" name="o" size="30" type="text" readonly />
-                      </div>
-                      <div class="grid-item">
-                          <?php echo lang('birthday');?><br />
-                          <input maxlength="20" id="birthday" name="birthday" size="15" type="date" required readonly />
-                      </div>
-                      <div class="grid-item">
-                        <?php echo lang('address');?><br />
-                        <input maxlength="50" id="address" name="address" size="60" type="text" readonly />
-                      </div>
-                      <div class="grid-item">
-                        <?php echo lang('phone');?><br />
-                        <input maxlength="10" id="phone" name="phone" size="15" type="text" readonly />
-                      </div>
-                      <div class="grid-item">
-                        <?php echo lang('card');?><br />
-                        <div id="cards"></div>
-                        <div id="card_selector">
-                          <?php echo form_dropdown('card_menu', $cards, '0', $card_attr);?>
-                        </div>
-                      </div>
-                  </form>
+                  <div class="grid-item">
+                      <div id="photo_bg" class="photo"><div id="photo_del" data-title="<?php echo lang('delete')?>" hidden><img src="/img/delete.png" /></div></div>
+                      <input id="photo" name="photo" type="file" hidden /><br />
+                      <?php echo lang('class');?><br />
+                      <?php echo form_dropdown('class', $classes, '0', $class_attr);?>
+                  </div>
+                  <div class="grid-item">
+                      <?php echo lang('f');?><br />
+                      <input maxlength="20" id="f" name="f" size="30" type="text" required readonly />
+                  </div>
+                  <div class="grid-item">
+                      <?php echo lang('i');?><br />
+                      <input maxlength="20" id="i" name="i" size="30" type="text" required readonly />
+                  </div>
+                  <div class="grid-item">
+                      <?php echo lang('o');?><br />
+                      <input maxlength="20" id="o" name="o" size="30" type="text" readonly />
+                  </div>
+                  <div class="grid-item">
+                      <?php echo lang('birthday');?><br />
+                      <input maxlength="20" id="birthday" name="birthday" size="15" type="date" required readonly />
+                  </div>
+                  <div class="grid-item">
+                    <?php echo lang('address');?><br />
+                    <input maxlength="50" id="address" name="address" size="60" type="text" readonly />
+                  </div>
+                  <div class="grid-item">
+                    <?php echo lang('phone');?><br />
+                    <input maxlength="10" id="phone" name="phone" size="15" type="text" readonly />
+                  </div>
+                  <div class="grid-item">
+                    <?php echo lang('card');?><br />
+                    <div id="cards"></div>
+                    <div id="card_selector">
+                      <?php echo form_dropdown('card_menu', $cards, '0', $card_attr);?>
+                    </div>
+                  </div>
                   <div class="grid-item">
                     <button id="save" type="button"><?php echo lang('save');?></button>
                   </div>
