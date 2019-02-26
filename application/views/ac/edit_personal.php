a673cb0a3488b1dc30b56d630b3941c4de7c80a4
Artem <artem.fufaldin@gmail.com>
2019-02-26 14:48:14 +0500

    Styling

--- application/views/ac/edit_personal.php
+++ application/views/ac/edit_personal.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-            <div id="main" class="main-container">
+            <div id="main" class="main-grid-container">
                     <div id="menu" onclick="tree_toggle(arguments[0]);">
                         <?php echo $menu;?>
                     </div>
