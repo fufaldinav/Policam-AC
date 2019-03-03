2cc756dd5dca4044b2b2bcdbe71ac6f28d37aa58
Artem <artem.fufaldin@gmail.com>
2019-03-03 05:05:01 +0500

    Move 'util -> controllers'

--- application/controllers/Util.php
+++ application/controllers/Util.php
@@ -94,53 +94,6 @@ class Util extends CI_Controller
 	}
 
 	/**
-	 * Выгрузка всех карт в контроллер
-	 *
-	 * @param  int  $controller_id
-	 */
-	public function reload($controller_id = null)
-	{
-		if (!$this->ion_auth->logged_in()) {
-			header("HTTP/1.1 401 Unauthorized");
-			exit;
-		}
-		if (!$this->ion_auth->is_admin()) {
-			header('HTTP/1.1 403 Forbidden');
-			exit;
-		}
-		if (isset($controller_id)) {
-			echo $this->ac->add_all_cards_to_controller($controller_id);
-			echo ' заданий записано'; //TODO
-		} else {
-			echo 'Не выбран контроллер'; //TODO
-		}
-	}
-
-	/**
-	 * Установка времени открытия
-	 *
-	 * @param  int  $controller_id
-	 * @param  int  $open_time
-	 */
-	public function door($controller_id = null, $open_time = null)
-	{
-		if (!$this->ion_auth->logged_in()) {
-			header("HTTP/1.1 401 Unauthorized");
-			exit;
-		}
-		if (!$this->ion_auth->is_admin()) {
-			header('HTTP/1.1 403 Forbidden');
-			exit;
-		}
-		if (isset($controller_id) && isset($open_time)) {
-			echo $this->ac->set_door_params($controller_id, $open_time);
-			echo ' заданий записано'; //TODO
-		} else {
-			echo 'Не выбран контроллер или не задано время открытия'; //TODO
-		}
-	}
-
-	/**
 	 * Обработка информации о проблеме с картой
 	 */
 	public function card_problem()
