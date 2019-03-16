9282623a7354b3cc9b1734a91e4e3e8ff4b8a8aa
Artem <artem.fufaldin@gmail.com>
2019-03-17 02:21:15 +0500

    Переработал Token_model

--- application/models/ac/Token_model.php
+++ application/models/ac/Token_model.php
@@ -20,77 +20,27 @@ defined('BASEPATH') or exit('No direct script access allowed');
 /**
  * Class Token Model
  */
-class Token_model extends CI_Model
+class Token_model extends MY_Model
 {
+    /**
+     * Пользователь, которому принадлежит токен
+     *
+     * @var int
+     */
+    public $user_id;
+
+    /**
+     * Токен, полученный от сервера FCM
+     *
+     * @var string
+     */
+    public $token;
+
     public function __construct()
     {
         parent::__construct();
 
-        $this->load->database();
-    }
-
-    /**
-     * Получает токен по ключу
-     *
-     * @param string $token_key Ключ
-     *
-     * @return object|null Токен
-     */
-    public function get(string $token_key): ?object
-    {
-        $query = $this->db
-            ->where('token', $token_key)
-            ->get('tokens');
-
-        return $query->row();
-    }
-
-    /**
-     * Получает все токены пользователя
-     *
-     * @param int|null $user_id ID пользователя
-     *
-     * @return object[] Массив с токенами или пустой массив
-     */
-    public function get_list(int $user_id = null): array
-    {
-        if (isset($user_id)) {
-            $this->db->where('user_id', $user_id);
-        }
-        $query = $this->db->get('tokens');
-
-        return $query->result();
-    }
-
-    /**
-     * Добавляет токен
-     *
-     * @param int    $user_id   ID пользователя
-     * @param string $token_key Токен
-     *
-     * @return int ID токена
-     */
-    public function add(int $user_id, string $token_key): int
-    {
-        $this->db->insert('tokens', [
-            'user_id' => $user_id,
-            'token' => $token_key
-        ]);
-
-        return $this->db->insert_id();
-    }
-
-    /**
-     * Удаление токена
-     *
-     * @param string $token_key Ключ
-     *
-     * @return int Количество успешных удалений
-     */
-    public function delete(string $token_key): int
-    {
-        $this->db->delete('users_tokens', ['token' => $token_key]);
-
-        return $this->db->affected_rows();
+        $this->_table = 'tokens';
+        $this->_foreing_key = 'user_id';
     }
 }
