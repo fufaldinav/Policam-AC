b6b7f0ef7f25bd8e26d12b66ec334a801532147f
Artem <artem.fufaldin@gmail.com>
2019-03-25 23:29:07 +0500

    Пустой проект Laravel

--- application/views/ac/cp.php
+++
@@ -1,9 +0,0 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');?>
-		<div id="main" class="main-grid-container">
-			<div id="menu">
-				<!-- MENU -->
-			</div>
-			<div id="info">
-				<button type="button" id="subscribe" onClick="subscribe();">Следить за изменениями</button>
-			</div>
-		</div>
