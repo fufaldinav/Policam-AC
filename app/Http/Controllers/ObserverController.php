7721a4b461b751a821bc9da851ea77b1a31d3067
Artem <artem.fufaldin@gmail.com>
2019-03-26 21:18:51 +0500

    Добавил Observer

---
+++ app/Http/Controllers/ObserverController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App, Auth;
+
+class ObserverController extends Controller
+{
+    public function index()
+    {
+        $user = Auth::user();
+
+        if (! isset($user)) {
+            return redirect('login');
+        }
+
+        $user = App\User::find($user->id);
+
+        $org = $user->organizations()->first();
+
+        /*
+        | Подразделения
+        */
+        $divs = $org
+            ->divisions()
+            ->orderBy('type')
+            ->orderByRaw('CAST(name AS UNSIGNED) ASC')
+            ->orderBy('name')
+            ->get();
+
+        $data = [
+            'divs' => $divs,
+            'org_name' => $org->name ?? 'Missing',
+            'css_list' => ['ac'],
+            'js_list' => ['main', 'observer']
+        ];
+
+
+        return view('ac.observer', $data);
+    }
+}
