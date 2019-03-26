c80f935fe7b018f497dc540b7fddc0e3824a77d1
m2jest1c <artem.fufaldin@gmail.com>
2019-03-26 17:04:43 +0500

    Добавил классу Person указание на конкретнкую таблицу

--- app/Person.php
+++ app/Person.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
+    protected $table = 'persons';
+
     public function cards()
     {
         return $this->hasMany('App\Card');
