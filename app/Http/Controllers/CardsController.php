944ff8ce9628573a28f552d4a2aaeee328f78009
m2jest1c <artem.fufaldin@gmail.com>
2019-03-27 11:51:45 +0500

    Добавил методы в Cards

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -8,5 +8,72 @@ use App, Auth;
 
 class CardsController extends Controller
 {
-    //
+    public function holder(int $card_id = null, int $person_id = 0)
+    {
+        if (is_null($card_id)) {
+            return 0;
+        }
+
+        $user = Auth::user();
+
+        if (! isset($user)) {
+            return 0;
+        }
+
+        $user = App\User::find($user->id);
+
+        $org = $user->organizations()->first();
+
+        $card = App\Card::find($card_id);
+        $card->person_id = $person_id;
+
+        $ctrls = $org->controllers;
+        if ($ctrls) {
+            if ($card->person_id == 0) {
+//                $this->task->delCards([$card->wiegand]);
+            } else {
+//                $this->task->addCards([$card->wiegand]);
+            }
+
+            foreach ($ctrls as $ctrl) {
+//                $this->task->add($ctrl->id);
+//                $this->task->send();
+            }
+        }
+
+
+        return $card->save();
+    }
+
+    public function delete(int $card_id = null)
+    {
+        $user = Auth::user();
+
+        if (! isset($user)) {
+            return 0;
+        }
+
+        if (is_null($card_id)) {
+            return 0;
+        }
+
+        $card = App\Card::find($card_id);
+
+        return $card->delete();
+    }
+
+    public function getList(int $person_id = 0)
+    {
+        $user = Auth::user();
+
+        if (! isset($user)) {
+            return 0;
+        }
+
+        $person = App\Person::find($person_id);
+
+//        header('Content-Type: application/json');
+
+        return json_encode($person->cards);
+    }
 }
