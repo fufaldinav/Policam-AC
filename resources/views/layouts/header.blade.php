e9e9cda63c9f54811345adaf1239b808e9b75690
Artem <artem.fufaldin@gmail.com>
2019-03-27 19:12:20 +0500

    Исправил кнопку выхода

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,9 +7,13 @@
             @include('layouts.nav')
         </div>
         <div id="header-right">
-            <a class="nav" href="{{ url('/logout') }}">
+            <a class="nav" href="{{ route('logout') }}" onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
                 @lang('ac/common.exit')
             </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
         </div>
     </div>
 </div>
