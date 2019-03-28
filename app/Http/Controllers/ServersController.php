5c0582b2767189c94b6050718c0d1e23c4421af7
Artem <artem.fufaldin@gmail.com>
2019-03-28 18:40:35 +0500

    Добавил в Servers контроллер прием POST от Z5RWEB

--- app/Http/Controllers/ServersController.php
+++ app/Http/Controllers/ServersController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Policam\Ac\Z5RWEB;
+
 class ServersController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $handler = new Z5RWEB\Request($request->getContent());
+        return $handler->handle();
+    }
 }
