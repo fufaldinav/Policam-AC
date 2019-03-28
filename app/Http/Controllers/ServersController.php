4e91fe56b0d8c8cf81fca3a3273f771ee60a24f8
Artem <artem.fufaldin@gmail.com>
2019-03-28 23:14:05 +0500

    Чистка

--- app/Http/Controllers/ServersController.php
+++ app/Http/Controllers/ServersController.php
@@ -8,6 +8,12 @@ use App\Policam\Ac\Z5RWEB;
 
 class ServersController extends Controller
 {
+    /**
+     * @param Request $request
+     *
+     * @return string|null
+     * @throws \Exception
+     */
     public function index(Request $request)
     {
         $handler = new Z5RWEB\Request($request->getContent());
