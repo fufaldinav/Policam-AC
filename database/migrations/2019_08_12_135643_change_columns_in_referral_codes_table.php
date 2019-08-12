e89c544d9486e46d9d5d0f22a344e11c1a4970e0
Artem <artem.fufaldin@gmail.com>
2019-08-12 15:00:20 +0500

    Изменил максимальное кол-во символов

---
+++ database/migrations/2019_08_12_135643_change_columns_in_referral_codes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeColumnsInReferralCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('referral_codes', function (Blueprint $table) {
+            $table->string('code', 13)->change();
+            $table->string('card', 8)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('referral_codes', function (Blueprint $table) {
+            $table->string('code', 32)->change();
+            $table->string('card')->change();
+        });
+    }
+}
