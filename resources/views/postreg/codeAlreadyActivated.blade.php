8222da5cf95ec2ff9284ea6998979c124ff674b3
Artem <artem.fufaldin@gmail.com>
2019-09-27 13:13:01 +0500

    Код не найден или уже активирован

--- resources/views/postreg/codeAlreadyActivated.blade.php
+++ resources/views/postreg/codeAlreadyActivated.blade.php
@@ -4,7 +4,7 @@
     <div class="container-fluid">
         <div class="row justify-content-center pt-lg-4">
             <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4 bg-white shadow-sm rounded p-2 d-flex flex-column">
-                <div class="mb-3"><h5 class="text-center">Код уже активирован</h5></div>
+                <div class="mb-3"><h5 class="text-center">Код не найден или уже активирован</h5></div>
                 <div class="d-flex justify-content-center"><a href="/" class="btn btn-primary">На главную</a></div>
             </div>
         </div>
