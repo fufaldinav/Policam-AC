f7cbd7a289b8a1a19c11aa4625baa7860fbf04e4
Artem <artem.fufaldin@gmail.com>
2019-11-29 13:59:55 +0500

    Изменил текст сообщения

--- resources/views/emails/controller-status.blade.php
+++ resources/views/emails/controller-status.blade.php
@@ -1,11 +1,17 @@
 @component('mail::message')
     Привет, {{ $user->name }}!<br>
-    Контроллер изменил статус:<br>
+    Контроллер {{$controller->sn}} изменил статус:<br>
     @php
         $devices = json_decode($controller->devices_status);
     @endphp
 
     @foreach($devices as $id => $device)
-        Slave {{ $id }}: {{ $device->timeout }}<br>
+        <p>Slave {{ $id }}
+        @if($device->timeout == 0)
+            появился в сети
+        @else
+            не в сети, {{ $device->timeout }} таймаутов
+        @endif
+        </p>
     @endforeach
 @endcomponent
